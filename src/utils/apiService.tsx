88730475964507620ed07f0670fe6a1338592a41
nikita3006 <nikitanegi3011@gmail.com>
2024-01-12 18:36:51 +0530

    testing fetch api

---
+++ src/utils/apiService.tsx
@@ -0,0 +1,15 @@
+const apiUrl = 'http://localhost:3000';
+
+export const fetchData = async (endpoint: string) => {
+  try {
+    const response = await fetch(`${apiUrl}/${endpoint}`);
+    if (!response.ok) {
+      throw new Error(`Error fetching data from API: ${response.statusText}`);
+    }
+   
+    return await response.json();
+  } catch (error) {
+    console.error('Error fetching data from API:', error);
+    throw error;
+  }
+};
